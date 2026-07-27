b9b81a5fbf7b97710d778cde7d3d4e496f625d4c
fredd1ex <freddie@gihtub.com>
2026-07-27 21:20:50 +0100

    testing other fallback

--- pretrain.py
+++ pretrain.py
@@ -16,7 +16,13 @@ import coolname
 import hydra
 import pydantic
 from omegaconf import DictConfig
-from adam_atan2 import AdamATan2
+try:
+    from adam_atan2 import AdamATan2
+except ModuleNotFoundError as error:
+    if error.name != "adam_atan2_backend":
+        raise
+    print("adam-atan2 CUDA backend is unavailable; falling back to torch.optim.AdamW.")
+    AdamATan2 = torch.optim.AdamW
 
 from puzzle_dataset import PuzzleDataset, PuzzleDatasetConfig, PuzzleDatasetMetadata
 from utils.functions import load_model_class, get_model_source_path
